881ea13115d9a0557a3960a221f74aaa937f9ae9
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-01-18 18:02:34 -0500

    added message for empty email

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ const form = document.querySelector(".form");
 const email = document.querySelector(".email");
 
 button.addEventListener("click", (event) => {
+  let x = 0;
   event.preventDefault();
-  if (!email.value.includes("@") || !email.value.includes(".")) {
-    email.classList.add("invalid");
+  if (email.value.length < 1) {
+    email.classList.add("empty-email");
+  } else if (!email.value.includes("@") || !email.value.includes(".")) {
+    email.classList.add("invalid-email");
   } else {
     form.submit();
   }
 });
 
 email.addEventListener("click", () => {
-  email.classList.remove("invalid");
+  email.classList.remove("invalid-email");
+  email.classList.remove("empty-email");
 });
