e942a135e9ec57dd9e6c9a61267e4272a429bd09
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-01-18 20:43:13 -0500

    used email validation function from stackoverflow

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-const button = document.querySelector(".button");
+// const button = document.querySelector(".button");
 const form = document.querySelector(".form");
 const email = document.querySelector(".email");
 
-button.addEventListener("click", (event) => {
-  let x = 0;
-  event.preventDefault();
-  if (email.value.length < 1) {
+const isValidEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailValue = form["email"].value;
+  if (!emailValue) {
     email.classList.add("empty-email");
-  } else if (!email.value.includes("@") || !email.value.includes(".")) {
+  } else if (!isValidEmail(emailValue)) {
     email.classList.add("invalid-email");
   } else {
     form.submit();
   }
 });
+
+// button.addEventListener("click", (event) => {
+//   let x = 0;
+//   event.preventDefault();
+//   if (email.value.length < 1) {
+//     email.classList.add("empty-email");
+//   } else if (!email.value.includes("@") || !email.value.includes(".")) {
+//     email.classList.add("invalid-email");
+//   } else {
+//     form.submit();
+//   }
+// });
 
 email.addEventListener("click", () => {
   email.classList.remove("invalid-email");
